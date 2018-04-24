880127db97f4a95ac32e3acd0589810220f51570
zhangDan <zhangdan@gmw.cn>
2018-04-24 11:01:41 +0800

    增加友盟消息推送和统计

--- app/src/main/java/com/example/administrator/antest1/MainActivity.java
+++ app/src/main/java/com/example/administrator/antest1/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.administrator.antest1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.umeng.analytics.MobclickAgent;
+import com.umeng.commonsdk.UMConfigure;
+import com.umeng.message.PushAgent;
 
 public class MainActivity extends Activity {
-
+    private Button btnOne;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //UM统计集成设置
+        UMConfigure.setLogEnabled(true);
+        UMConfigure.setEncryptEnabled(true);
+        MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL);
+        PushAgent.getInstance(this).onAppStart();
+        //UM统计集成设置
+        btnOne = (Button)findViewById(R.id.btnOne);
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnOne.setText("hello!");
+            }
+        });
+    }
+    //设置UM统计集成
+    @Override
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
     }
 }
